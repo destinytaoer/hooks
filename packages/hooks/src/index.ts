f75f58cc4cc94d42e41a0a15442e88c42f3a1d02
destiny <1848765519@qq.com>
2023-01-18 19:19:57 +0800

    feat: use-latest-ref

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,4 @@
 export { useMount } from './hooks/use-mount';
 export { useUpdate } from './hooks/use-update';
 export { useIsMountedRef } from './hooks/use-is-mounted-ref';
+export { useLatestRef } from './hooks/use-latest-ref';

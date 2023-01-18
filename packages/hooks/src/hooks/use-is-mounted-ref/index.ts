b32037a26a0341d5a377d1166bd492ffe87c4f36
destiny <1848765519@qq.com>
2023-01-18 19:07:47 +0800

    feat: use-is-mounted-ref

---
+++ packages/hooks/src/hooks/use-is-mounted-ref/index.ts
@@ -0,0 +1,14 @@
+import { useEffect, useRef } from 'react';
+
+export const useIsMountedRef = () => {
+  const isMountedRef = useRef(false);
+
+  useEffect(() => {
+    isMountedRef.current = true;
+    return () => {
+      isMountedRef.current = false;
+    };
+  }, []);
+
+  return isMountedRef;
+};
--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,2 +1,3 @@
 export { useMount } from './hooks/use-mount';
 export { useUpdate } from './hooks/use-update';
+export { useIsMountedRef } from './hooks/use-is-mounted-ref';

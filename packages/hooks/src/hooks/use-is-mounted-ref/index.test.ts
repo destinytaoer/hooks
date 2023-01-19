c82ebca6c721418750f3d7d31a111e293d2260f0
destiny <1848765519@qq.com>
2023-01-19 10:07:02 +0800

    test: update use-is-mounted-ref test case

--- packages/hooks/src/hooks/use-is-mounted-ref/index.test.ts
+++ packages/hooks/src/hooks/use-is-mounted-ref/index.test.ts
@@ -1,13 +1,21 @@
 import { renderHook } from '@testing-library/react';
 import { useIsMountedRef } from './index';
 
-describe("useIsMountedRef", () => {
-    it('test mount', async () =>{
-        const hook = renderHook(() => useIsMountedRef())
-        expect(hook.result.current.current).toBe(true)
-        hook.rerender()
-        expect(hook.result.current.current).toBe(true)
-        hook.unmount()
-        expect(hook.result.current.current).toBe(false)
-    })
-})
+describe('useIsMountedRef', () => {
+  it('should be true when mounted', async () => {
+    const hook = renderHook(() => useIsMountedRef());
+    expect(hook.result.current.current).toBe(true);
+  });
+  it('should be true when rerender', async () => {
+    const hook = renderHook(() => useIsMountedRef());
+    expect(hook.result.current.current).toBe(true);
+    hook.rerender();
+    expect(hook.result.current.current).toBe(true);
+  });
+  it('should be false when unmounted', async () => {
+    const hook = renderHook(() => useIsMountedRef());
+    expect(hook.result.current.current).toBe(true);
+    hook.unmount();
+    expect(hook.result.current.current).toBe(false);
+  });
+});

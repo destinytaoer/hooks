25f86db1eb94b1ac50c51f28e36af8e414c6fc42
destiny <1848765519@qq.com>
2023-01-28 17:09:38 +0800

    feat: use-creation

---
+++ packages/hooks/src/hooks/use-creation/index.ts
@@ -0,0 +1,37 @@
+import { DependencyList, useRef } from 'react';
+
+// 用于创建开销较大的对象, 避免每次渲染都重新创建
+// export const useCreation = <T>(factory: () => T) => {
+//   const ref = useRef<T>();
+//   if (!ref.current) {
+//     ref.current = factory();
+//   }
+//   return ref.current;
+// }
+
+export default function depsAreSame(oldDeps: DependencyList, deps: DependencyList): boolean {
+  if (oldDeps === deps) return true;
+  for (let i = 0; i < oldDeps.length; i++) {
+    if (!Object.is(oldDeps[i], deps[i])) return false;
+  }
+  return true;
+}
+
+// ahooks 进阶版, 增加依赖
+// useMemo 不能保证被 memo 的值一定不会被重计算
+// useRef 没有惰性初始化, 存在性能开销
+export const useCreation = <T>(factory: () => T, deps: DependencyList) => {
+  const { current } = useRef({
+    deps,
+    value: undefined as undefined | T,
+    initialize: false,
+  });
+
+  if (!current.initialize || !depsAreSame(current.deps, deps)) {
+    current.deps = deps;
+    current.value = factory();
+    current.initialize = true;
+  }
+
+  return current.value as T;
+};

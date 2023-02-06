064cdb7a0fe840dd801958b8e7800783119fe1fc
destiny <1848765519@qq.com>
2023-02-06 16:23:19 +0800

    feat: useMemoizedFn

---
+++ packages/hooks/src/hooks/use-memoized-fn/index.ts
@@ -0,0 +1,32 @@
+import { useMemo, useRef } from 'react';
+
+type noop = (this: any, ...args: any[]) => any;
+
+type PickFunction<T extends noop> = (
+  this: ThisParameterType<T>,
+  ...args: Parameters<T>
+) => ReturnType<T>;
+
+function useMemoizedFn<T extends noop>(fn: T) {
+  if (typeof fn !== 'function') {
+    console.error(`useMemoizedFn expected parameter is a function, got ${typeof fn}`);
+  }
+
+  const fnRef = useRef<T>(fn);
+
+  // why not write `fnRef.current = fn`?
+  // https://github.com/alibaba/hooks/issues/728
+  // 为了解决 React devtools 的问题, 使用 useMemo 包裹起来
+  fnRef.current = useMemo(() => fn, [fn]);
+
+  const memoizedFn = useRef<PickFunction<T>>();
+  if (!memoizedFn.current) {
+    memoizedFn.current = function (this, ...args) {
+      return fnRef.current.apply(this, args);
+    };
+  }
+
+  return memoizedFn.current as T;
+}
+
+export default useMemoizedFn;

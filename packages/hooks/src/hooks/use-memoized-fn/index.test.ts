bf7faae161b0c14745cbafd16a0e6e4e418510e4
destiny <1848765519@qq.com>
2023-02-06 16:23:31 +0800

    test: test useMemoizedFn

---
+++ packages/hooks/src/hooks/use-memoized-fn/index.test.ts
@@ -0,0 +1,30 @@
+import { act, renderHook } from '@testing-library/react';
+import { useState } from 'react';
+import useMemoizedFn from './index';
+
+describe('useMemoizedFn', () => {
+  const useCount = () => {
+    const [count, setCount] = useState(0);
+
+    const addCount = () => {
+      setCount((c) => c + 1);
+    };
+
+    const memoizedFn = useMemoizedFn(() => count);
+
+    return { addCount, memoizedFn };
+  };
+
+  it('useMemoizedFn should work', () => {
+    const hook = renderHook(() => useCount());
+    const currentFn = hook.result.current.memoizedFn;
+    expect(hook.result.current.memoizedFn()).toEqual(0);
+
+    act(() => {
+      hook.result.current.addCount();
+    });
+
+    expect(currentFn).toEqual(hook.result.current.memoizedFn);
+    expect(hook.result.current.memoizedFn()).toEqual(1);
+  });
+});
